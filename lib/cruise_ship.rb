346b29d23105a9758580b3869b7ea5462dbd7fb5
Tommy Werner <wtwerner@gmail.com>
2020-05-14 00:06:12 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,10 +7,12 @@
 # }
 
 def select_winner(passengers)
-  winner = ""
+winner = ""
 passengers.each do |suite, name|
   if suite == :suite_a && name.start_with?("A")
     winner = name
   end
+end
+ 
 winner
 end
