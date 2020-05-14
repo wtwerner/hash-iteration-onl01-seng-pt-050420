2d708554a6d5b90991dd48c461782e47ca1be993
Tommy Werner <wtwerner@gmail.com>
2020-05-14 00:10:44 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,9 +6,7 @@
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-    if age < 13
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-  end
   end
 end
 
